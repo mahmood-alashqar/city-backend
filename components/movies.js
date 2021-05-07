2c4464e9db20d2887fccbc4f2ec96dd61b05cf1f
mahmood-alashqar <mahmood.ashqur@hotmail.com>
2021-05-07 03:10:53 +0300

    img shown

--- components/movies.js
+++ components/movies.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const handelMovies =(req , res) => {
     try {
     const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIES_KEY}&query=${req.query.query}`;
-    console.log(req.query.query);
+    console.log(process.env.REACT_APP_MOVIES_KEY);
     superagent.get(url).then( data => {
         const moviesData = data.body.results.map( data1 => new Movies(data1));
         res.send(moviesData);
@@ -29,7 +29,7 @@ const handelMovies =(req , res) => {
        this.average_votes =data.vote_average;
        this.total_votes=data.vote_count ;
        if(data.backdrop_path !== null)
-           this.image_url =`https://image.tmdb.org/t/p/w500/${data.backdrop_path}`;
+           this.image_url =`https://image.tmdb.org/t/p/w500${data.backdrop_path}`;
        else
            this.image_url = 'there is no image';
        this.popularity = data.popularity ;
